617f522763590d6a9a5690c817177bc03f97cc88
Nir <99nirc99@gmail.com>
2021-02-06 18:54:27 +0200

    Logged charge statuses route

--- src/controllers/charge-statuses.controller.ts
+++ src/controllers/charge-statuses.controller.ts
@@ -1,10 +1,11 @@
 import {Request, Response} from "express";
 import {getDeclinedChargesForMerchant} from "../models/charge-statuses.model";
+import logger from "../util/logger";
 
 const chargeStatusesController = {
   get: (req: Request, res: Response) => {
     const merchant_identifier = req.header('merchant_identifier');
-
+    logger.info(`Fetching declined charges statuses for merchant: ${merchant_identifier}`);
     res.send(getDeclinedChargesForMerchant(merchant_identifier));
   }
 }
